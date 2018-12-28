2296997da6c878c7723d452a8bc0765d6a008027
Pramuditha Aravinda <aravindadp@gmail.com>
2018-12-28 13:46:14 +0530

    style: Fix C++ style
    Fix C++ style issues in C++ code.

--- src/TwiddlingPID.cpp
+++ src/TwiddlingPID.cpp
@@ -26,25 +26,25 @@ void TwiddlingPID::UpdateError(double cte) {
   PID::UpdateError(cte);
   i_++;
   if (i_ > n_) {
-	  error_ += cte * cte;
+    error_ += cte * cte;
   }
 }
 
 double TwiddlingPID::TotalError() {
   double pid_error = PID::TotalError();
 
-	double dp_sum = std::accumulate(dp_, dp_+3, 0.0);
+  double dp_sum = std::accumulate(dp_, dp_+3, 0.0);
   if (dp_sum < 0.001){ //Twiddling finished. Continue;
     return pid_error;
   }
 
   if (abs(p_error) > max_cte_ && i_ > 10) { // Crashed
-	  pid_error = std::numeric_limits<double>::quiet_NaN();
-	  Twiddle();
+    pid_error = std::numeric_limits<double>::quiet_NaN();
+    Twiddle();
   }
   else if (i_ > 2 * n_) { // Success run
-	  pid_error = std::numeric_limits<double>::quiet_NaN();
-	  Twiddle();
+    pid_error = std::numeric_limits<double>::quiet_NaN();
+    Twiddle();
   }
 
   return pid_error;
@@ -53,37 +53,37 @@ double TwiddlingPID::TotalError() {
 void TwiddlingPID::Twiddle() {
   double err = 0;
   if (i_ <= n_) {
-	  err = max_cte_*max_cte_;
+    err = max_cte_*max_cte_;
   }
   else {
-	  err = error_*n_ / (i_*(i_-n_));
+    err = error_*n_ / (i_*(i_-n_));
   }
 
   //Re-warped twiddle algorithm
   if (increment_) { 
-	  if (err < best_error) {
+    if (err < best_error) {
       best_error = err;
-	    dp_[tuning_idx_] *= 1.1;
-	    NextCoefficient();
-	    UpdateCoefficient(dp_[tuning_idx_]);
-	  }
-	  else {
-	    UpdateCoefficient(- dp_[tuning_idx_] * 2);
-	    increment_ = false;
-	  }
+      dp_[tuning_idx_] *= 1.1;
+      NextCoefficient();
+      UpdateCoefficient(dp_[tuning_idx_]);
+    }
+    else {
+      UpdateCoefficient(- dp_[tuning_idx_] * 2);
+      increment_ = false;
+    }
   }
   else {
-	  if (err < best_error) {
-		  best_error = err;
-		  dp_[tuning_idx_] *= 1.1;
-	  }
-	  else {
-		  UpdateCoefficient(dp_[tuning_idx_]);
-		  dp_[tuning_idx_] *= 0.9;
-	  }
-	  NextCoefficient();
-	  UpdateCoefficient(dp_[tuning_idx_]);
-	  increment_ = true;
+    if (err < best_error) {
+      best_error = err;
+      dp_[tuning_idx_] *= 1.1;
+    }
+    else {
+      UpdateCoefficient(dp_[tuning_idx_]);
+      dp_[tuning_idx_] *= 0.9;
+    }
+    NextCoefficient();
+    UpdateCoefficient(dp_[tuning_idx_]);
+    increment_ = true;
   }
   error_ = 0;
   i_ = 0;
@@ -94,18 +94,18 @@ void TwiddlingPID::Twiddle() {
 }
 
 void TwiddlingPID::UpdateCoefficient(double dp) {
-	switch (tuning_idx_) {
-	  case 0:
-		  Kp += dp;
-		  break;
-	  case 1:
-		  Ki += dp;
-		  break;
-	  case 2:
-		  Kd += dp;
-		  break;
-	  default:
-		  break;
-	}
+  switch (tuning_idx_) {
+    case 0:
+      Kp += dp;
+      break;
+    case 1:
+      Ki += dp;
+      break;
+    case 2:
+      Kd += dp;
+      break;
+    default:
+      break;
+  }
 }
 
